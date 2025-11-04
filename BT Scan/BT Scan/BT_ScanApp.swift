fc91e7d7e30bc1fe431311cb5328563d8ea6c860
Anton <a.i.duda31@gmail.com>
2025-11-04 10:38:12 +0200

    Initial Xcode project

---
+++ BT Scan/BT Scan/BT_ScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  BT_ScanApp.swift
+//  BT Scan
+//
+//  Created by Anton.Duda on 04.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BT_ScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BT Scan/BT Scan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BT Scan
+//
+//  Created by Anton.Duda on 04.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
