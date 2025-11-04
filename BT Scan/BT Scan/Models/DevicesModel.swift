d7171667d9c6ad6980de14006bbf2142cfc004a8
Anton <a.i.duda31@gmail.com>
2025-11-04 10:44:46 +0200

    Implemented models for the device and device characteristics

---
+++ BT Scan/BT Scan/Models/DevicesModel.swift
@@ -0,0 +1,15 @@
+//
+//  DevicesModel.swift
+//  BT Scan
+//
+//  Created by Anton.Duda on 04.11.2025.
+//
+
+import CoreBluetooth
+
+struct DevicesModel: Identifiable, Hashable {
+    let id = UUID()
+    let peripheral: CBPeripheral
+    let name: String
+    let rssi: Int
+}
